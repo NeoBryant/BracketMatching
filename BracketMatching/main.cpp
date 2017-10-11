2e8ba90d8314b7699a91ead48927bbf8eddb3ead
NeoBryant <1512917808@qq.com>
2017-10-11 08:51:06 +0800

    first commit

--- BracketMatching/main.cpp
+++ BracketMatching/main.cpp
@@ -7,9 +7,97 @@
 //
 
 #include <iostream>
+#include <cstring>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+// 1  表示 Yes
+// 0  表示 No
+// -1 表示 UnKnown
+
+int main(){
+    int i = 0;
+    int num = 0;
+    bool IsNoMatch = false;
+    string str;
+    struct stack
+    {
+        int len;
+        string element;
+    } SysmStack;  // 栈 后进先出  i 记录 栈顶元素
+    
+    SysmStack.len = 0;
+    cout << "请输入实验次数: " ;
+    cin >> num;
+    
+    
+    for (int j = 0; j < num; ++j)
+    {
+        i = 0;
+        SysmStack.len = 0;
+        IsNoMatch = true;
+        
+        cout << "第 " << j+1 << " 次实验，请输入一串字符: ";
+        cin >> str;
+        
+        while (str[i] == '[' || str[i] == ']' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}'){
+            
+            IsNoMatch = true;
+            
+            switch (str[i])
+            {
+                case '[':  // 入栈
+                case '(':
+                case '{':
+                    SysmStack.element[SysmStack.len] = str[i];
+                    SysmStack.len ++;
+                    break;
+                case ']':
+                    if (SysmStack.element[SysmStack.len-1] == '[') {
+                        // 出栈
+                        SysmStack.element[SysmStack.len-1] = '\0';
+                        SysmStack.len--;
+                    }
+                    else {
+                        // 输出 No
+                        IsNoMatch = false;
+                        break;
+                    }
+                    break;
+                case ')':
+                    if (SysmStack.element[SysmStack.len-1] == '(') {
+                        // 出栈
+                        SysmStack.element[SysmStack.len-1] = '\0';
+                        SysmStack.len--;
+                    }
+                    else {
+                        // 输出 No
+                        IsNoMatch = false;
+                        break;
+                    }
+                    break;
+                case '}':
+                    if (SysmStack.element[SysmStack.len-1] == '{') {
+                        // 出栈
+                        SysmStack.element[SysmStack.len-1] = '\0';
+                        SysmStack.len--;
+                    }
+                    else {
+                        // 输出 No
+                        IsNoMatch = false;
+                        break;
+                    }
+                    break;
+            }
+            i++;
+            
+            if (!IsNoMatch) {
+                break;
+            }
+        }
+        if (IsNoMatch && SysmStack.len == 0)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
 }
+
